372729c25ba615faba805fd6512ef38547198a8e
Mitchell Foote <mcfoote321@gmail.com>
2022-11-16 23:35:52 -0500

    work on models

---
+++ models/Thought.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtsSchema = new Schema({
+    thoughtText:{
+        type: String,
+        required: true,
+        min_length: 1,
+        max_length: 280,
+    },
+    createdAt: {
+        type: Date,
+        //set default
+        //getter to format on query
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: {
+        type: Array[docs],
+    },
+});
