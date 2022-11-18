559962fec0cec62a6692f78e6baa2ba9ce925308
Mitchell Foote <mcfoote321@gmail.com>
2022-11-18 18:09:24 -0500

    working on server

---
+++ server.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const PORT = process.nextTick.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
