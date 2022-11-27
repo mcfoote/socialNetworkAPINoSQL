69f8a161bf2981b7a1540aadf04a63d0ca4fd70f
Mitchell Foote <mcfoote321@gmail.com>
2022-11-26 20:17:01 -0500

    more work on controllers

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,16 +1,48 @@
-const { Thoughts } = require('../models');
+const { Thought } = require('../models');
 
 const thoughtsController = {
 
     createNewThought({params, body}, res) {
 
+        Thought.create(body)
+        .then(({_id}) => {
+            return User.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        }).then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'Cannot find thoughts with matching ID'});
+                return;
+            }res.json(dbThoughtsData)
+        }).catch(err => res.status(400).json(err));
+
     },
 
     getThoughts(req, res) {
 
+        Thought.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => res.json(dbThoughtsData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+
     },
 
     getThoughtsByID({params}, res) {
+
+        Thought.findOne({ _id: params.id })
+        .populate({path: 'reactions',select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+            res.status(404).json({message: 'Cannot find thoughts with matching ID'});
+            return;
+        }res.json(dbThoughtsData)
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
 
     },
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,16 @@ const { User } = require('../models');
 const userController = {
 
     createNewUser({body}, res) {
+
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
+
     },
 
     getUsers(req, res) {
-        Users.find({})
+
+        User.find({})
         .populate({path:'thoughts', select:'-__v'})
         .populate({path:'friends', select:'-__v'})
         .select('-__v')
@@ -18,9 +21,24 @@ const userController = {
             console.log(err);
             res.status(400).json(err);
         });
+
     },
 
     getUserByID({params}, res) {
+
+        User.findOne({_id: params.id })
+        .populate({path:'thoughts', select:'-__v'})
+        .populate({path:'friends', select:'-__v'})
+        .select('-__v')
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message:'Cannot find user with matching ID'});
+                return; 
+            }res.json(dbUserData)
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 
     },
 
