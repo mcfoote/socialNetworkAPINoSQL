819852eb61a0571fe7eb2813ce357b681fcf5517
Mitchell Foote <mcfoote321@gmail.com>
2022-11-27 16:59:58 -0500

    move connection to config

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkAPI', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    }
-);
-
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}...`);
